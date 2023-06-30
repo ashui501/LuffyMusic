6e0a36d8a1f9e309cef321062f086ea84f2ebc18
Anonymous <adi855053@gmail.com>
2023-06-30 21:32:29 +0530

    LuffyMusic

--- LuffyMusic/plugins/reload.py
+++ LuffyMusic/plugins/reload.py
@@ -5,13 +5,13 @@ from pyrogram.types import CallbackQuery, Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, adminlist, lyrical
 from strings import get_command
-from AnonX import app
-from AnonX.core.call import Anon
-from AnonX.misc import db
-from AnonX.utils.database import get_authuser_names, get_cmode
-from AnonX.utils.decorators import (ActualAdminCB, AdminActual,
+from LuffyMusic import app
+from LuffyMusic.core.call import Anon
+from LuffyMusic.misc import db
+from LuffyMusic.utils.database import get_authuser_names, get_cmode
+from LuffyMusic.utils.decorators import (ActualAdminCB, AdminActual,
                                          language)
-from AnonX.utils.formatters import alpha_to_int
+from LuffyMusic.utils.formatters import alpha_to_int
 
 ### Multi-Lang Commands
 RELOAD_COMMAND = get_command("RELOAD_COMMAND")
