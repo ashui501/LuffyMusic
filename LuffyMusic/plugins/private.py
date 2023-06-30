cd2f5c311848a4692bb59e2cbbdc05ae283d8c0c
Anonymous <adi855053@gmail.com>
2023-06-30 21:31:37 +0530

    LuffyMusic

--- LuffyMusic/plugins/private.py
+++ LuffyMusic/plugins/private.py
@@ -3,13 +3,13 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (add_private_chat,
+from LuffyMusic import app
+from LuffyMusic.misc import SUDOERS
+from LuffyMusic.utils.database import (add_private_chat,
                                        get_private_served_chats,
                                        is_served_private_chat,
                                        remove_private_chat)
-from AnonX.utils.decorators.language import language
+from LuffyMusic.utils.decorators.language import language
 
 AUTHORIZE_COMMAND = get_command("AUTHORIZE_COMMAND")
 UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
