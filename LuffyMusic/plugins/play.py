70e95d60b41767842d3a9f005ad9e48beb451676
Anonymous <adi855053@gmail.com>
2023-06-30 21:29:30 +0530

    LuffyMusic

--- LuffyMusic/plugins/play.py
+++ LuffyMusic/plugins/play.py
@@ -10,21 +10,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from LuffyMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from AnonX.core.call import Anon
-from AnonX.utils import seconds_to_min, time_to_seconds
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.database import is_video_allowed
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.decorators.play import PlayWrapper
-from AnonX.utils.formatters import formats
-from AnonX.utils.inline.play import (livestream_markup,
+from LuffyMusic.core.call import Anon
+from LuffyMusic.utils import seconds_to_min, time_to_seconds
+from LuffyMusic.utils.channelplay import get_channeplayCB
+from LuffyMusic.utils.database import is_video_allowed
+from LuffyMusic.utils.decorators.language import languageCB
+from LuffyMusic.utils.decorators.play import PlayWrapper
+from LuffyMusic.utils.formatters import formats
+from LuffyMusic.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from AnonX.utils.inline.playlist import botplaylist_markup
-from AnonX.utils.logger import play_logs
-from AnonX.utils.stream.stream import stream
+from LuffyMusic.utils.inline.playlist import botplaylist_markup
+from LuffyMusic.utils.logger import play_logs
+from LuffyMusic.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
