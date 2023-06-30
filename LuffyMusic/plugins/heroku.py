dfb8c24733f6dfd5422cfcb80bd8a2054a85119b
Anonymous <adi855053@gmail.com>
2023-06-30 21:07:11 +0530

    LuffyMusic

--- LuffyMusic/plugins/heroku.py
+++ LuffyMusic/plugins/heroku.py
@@ -15,13 +15,13 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import HAPP, SUDOERS, XCB
-from AnonX.utils.database import (get_active_chats,
+from LuffyMusic import app
+from LuffyMusic.misc import HAPP, SUDOERS, XCB
+from LuffyMusic.utils.database import (get_active_chats,
                                        remove_active_chat,
                                        remove_active_video_chat)
-from AnonX.utils.decorators.language import language
-from AnonX.utils.pastebin import Anonbin
+from LuffyMusic.utils.decorators.language import language
+from LuffyMusic.utils.pastebin import Anonbin
 
 # Commands
 GETLOG_COMMAND = get_command("GETLOG_COMMAND")
