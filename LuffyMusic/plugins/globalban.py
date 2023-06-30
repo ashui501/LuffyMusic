14a5329ea55f559d0f503787a6c6b2143f645cd5
Anonymous <adi855053@gmail.com>
2023-06-30 21:05:17 +0530

    LuffyMusic

--- LuffyMusic/plugins/globalban.py
+++ LuffyMusic/plugins/globalban.py
@@ -7,16 +7,16 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils import get_readable_time
-from AnonX.utils.database import (add_banned_user,
+from LuffyMusic import app
+from LuffyMusic.misc import SUDOERS
+from LuffyMusic.utils import get_readable_time
+from LuffyMusic.utils.database import (add_banned_user,
                                        get_banned_count,
                                        get_banned_users,
                                        get_served_chats,
                                        is_banned_user,
                                        remove_banned_user)
-from AnonX.utils.decorators.language import language
+from LuffyMusic.utils.decorators.language import language
 
 # Command
 GBAN_COMMAND = get_command("GBAN_COMMAND")
