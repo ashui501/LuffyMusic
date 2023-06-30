55ffda70ad8b8c18f26c255866303069f5736335
Anonymous <adi855053@gmail.com>
2023-06-30 21:03:11 +0530

    LuffyMusic

--- LuffyMusic/plugins/channel.py
+++ LuffyMusic/plugins/channel.py
@@ -3,9 +3,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import app
-from AnonX.utils.database import set_cmode
-from AnonX.utils.decorators.admins import AdminActual
+from LuffyMusic import app
+from LuffyMusic.utils.database import set_cmode
+from LuffyMusic.utils.decorators.admins import AdminActual
 
 ### Multi-Lang Commands
 CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
