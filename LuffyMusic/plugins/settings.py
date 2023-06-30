6c009094e80b06cdeb0c984ffef7540e45e1b7bb
Anonymous <adi855053@gmail.com>
2023-06-30 21:34:38 +0530

    LuffyMusic

--- LuffyMusic/plugins/settings.py
+++ LuffyMusic/plugins/settings.py
@@ -6,8 +6,8 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
 from config import (BANNED_USERS, CLEANMODE_DELETE_MINS,
                     MUSIC_BOT_NAME, OWNER_ID)
 from strings import get_command
-from AnonX import app
-from AnonX.utils.database import (add_nonadmin_chat,
+from LuffyMusic import app
+from LuffyMusic.utils.database import (add_nonadmin_chat,
                                        cleanmode_off, cleanmode_on,
                                        commanddelete_off,
                                        commanddelete_on,
@@ -22,13 +22,13 @@ from AnonX.utils.database import (add_nonadmin_chat,
                                        save_audio_bitrate,
                                        save_video_bitrate,
                                        set_playmode, set_playtype)
-from AnonX.utils.decorators.admins import ActualAdminCB
-from AnonX.utils.decorators.language import language, languageCB
-from AnonX.utils.inline.settings import (
+from LuffyMusic.utils.decorators.admins import ActualAdminCB
+from LuffyMusic.utils.decorators.language import language, languageCB
+from LuffyMusic.utils.inline.settings import (
     audio_quality_markup, auth_users_markup,
     cleanmode_settings_markup, playmode_users_markup, setting_markup,
     video_quality_markup)
-from AnonX.utils.inline.start import private_panel
+from LuffyMusic.utils.inline.start import private_panel
 
 ### Command
 SETTINGS_COMMAND = get_command("SETTINGS_COMMAND")
