109fab5752b5249c1b58c00579fd415e302e8372
Anonymous <adi855053@gmail.com>
2023-06-30 23:39:33 +0530

    Initial commit

--- LuffyMusic/plugins/pause.py
+++ LuffyMusic/plugins/pause.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from LuffyMusic import app
-from LuffyMusic.core.call import Anon
+from LuffyMusic.core.call import Luffy
 from LuffyMusic.utils.database import is_music_playing, music_off
 from LuffyMusic.utils.decorators import AdminRightsCheck
 from LuffyMusic.utils.inline.play import close_keyboard
@@ -26,7 +26,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anon.pause_stream(chat_id)
+    await Luffy.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.first_name),
         reply_markup=close_keyboard
