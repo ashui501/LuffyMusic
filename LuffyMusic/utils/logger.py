4706f524725bf71411d39e78be2ac5230968e1ed
Anonymous <adi855053@gmail.com>
2023-06-30 22:09:34 +0530

    LuffyMusic

--- LuffyMusic/utils/logger.py
+++ LuffyMusic/utils/logger.py
@@ -1,6 +1,6 @@
 from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
-from AnonX import app
-from AnonX.utils.database import is_on_off
+from LuffyMusic import app
+from LuffyMusic.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
